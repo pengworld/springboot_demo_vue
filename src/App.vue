23d280eb7a6fbf9b7969c6b7ac97d2eeee266d2d
bigpeng <1015792002@qq.com>
2024-07-14 14:54:30 +0800

    by peng

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
   </div>
 </template>
 
@@ -13,12 +12,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
