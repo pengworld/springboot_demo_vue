ecd0cda2f9bc9ff6c63741a803e45a7c71988496
bigpeng <1015792002@qq.com>
2024-07-14 16:28:09 +0800

    by peng

--- src/components/view/register.vue
+++ src/components/view/register.vue
@@ -1,15 +1,55 @@
 <template>
-  <div>
-    haha
+  <div class="container">
+    <el-form  ref="userForm" :model="user" size="mini" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="user.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码">
+        <el-input v-model="user.password" type="password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="register">注册</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
   name: "user_register",
+  data(){
+    return{
+      user:{
+        username:"",
+        password:"",
+      },
+    }
+  },
+  methods:{
+    register(){
+      this.$axios.post(
+          "/login/register",
+          {
+            username:this.user.username,
+            password:this.user.password
+          }
+      )
+          .then((res)=>{
+            alert(res.data.msg)
+            console.log(res)
+          })
+          .catch(err=>{
+            console.log(err)
+          })
+    },
+  }
 }
 </script>
 
 <style scoped>
-
+.container {
+  width:25%;
+}
 </style>
